155a3f547f9e8d94678135def4eea03b04d40226
Alexander Paul P. Quinit <paulquinit@gmail.com>
2020-03-17 17:42:41 +0800

    Update sign method

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ class TwalaSigner {
     this.web3 = new Web3(provider)
   }
 
-  async signData (privateKey, data) {
-    return 'SIGNED'
+  async sign (privateKey, data) {
+    const hexedData = await this.web3.utils.asciiToHex(data)
+    const signature = await this.web3.eth.accounts.sign(hexedData, privateKey)
+    const message = signature.message
+    const messageHash = signature.messageHash
+    const v = signature.v
+    const r = signature.r
+    const s = signature.s
+    
+    return {
+      message: message,
+      message_hash: messageHash,
+      v: v,
+      r: r,
+      s: s
+    }
   }
 }
 
