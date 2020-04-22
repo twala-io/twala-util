b2ad972b93b2e1fc6c8f8c6665be5ff955015e3b
Alexander Paul P. Quinit <paulquinit@gmail.com>
2020-04-22 23:29:57 +0800

    Update methods

--- README.md
+++ README.md
@@ -1,14 +1,50 @@
-# Twala Signer
+# Twala Util
 
-Package for signing Twala data.
+A collection of utility functions for Twala.
 
-![Node.js CI](https://github.com/twala-io/twala-signer/workflows/Node.js%20CI/badge.svg)
-[![Version](https://img.shields.io/npm/v/twala-signer.svg)](https://npmjs.org/package/twala-signer)
-[![Downloads/week](https://img.shields.io/npm/dw/twala-signer.svg)](https://npmjs.org/package/twala-signer)
-[![License](https://img.shields.io/npm/l/twala-signer.svg)](https://github.com/twala-io/twala-signer/blob/master/package.json)
+![Node.js CI](https://github.com/twala-io/twala-util/workflows/Node.js%20CI/badge.svg)
+[![Version](https://img.shields.io/npm/v/twala-util.svg)](https://npmjs.org/package/twala-util)
+[![Downloads/week](https://img.shields.io/npm/dw/twala-util.svg)](https://npmjs.org/package/twala-util)
+[![License](https://img.shields.io/npm/l/twala-util.svg)](https://github.com/twala-io/twala-util/blob/master/package.json)
 
-# Installation
+## Features
+
+* Generate signature
+```js
+const TwalaUtil = require('twala-util')
+const util = new TwalaUtil(web3Provider)
+const signature = await util.sign('sample data', privateKey)
+```
+
+* Verify signature
+```js
+const TwalaUtil = require('twala-util')
+const util = new TwalaUtil(web3Provider)
+const isSigned = await util.isSigned(address, messageHash, v, r, s)
+```
+
+* Recover address
+```js
+const TwalaUtil = require('twala-util')
+const util = new TwalaUtil(web3Provider)
+const address = await util.recoverAddress(messageHash, v, r, s)
+```
+
+## Installation
 
 ```sh-session
-$ npm install twala-signer
+$ npm install twala-util
 ```
+
+## Contributing
+
+Keep it simple. Keep it minimal. Don't put every single feature just because you can.
+
+## Authors
+
+* Alexander Paul P. Quinit
+* Edilberto Salvador D. Salvador
+
+## License
+
+This project is licensed under the MIT License.
--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 const Web3 = require('web3')
 
-class TwalaSigner {
+class TwalaUtil {
   constructor (provider) {
     this.web3 = new Web3(provider)
+    this.signatureVerifierContractAbi = [{'inputs':[{'internalType':'address','name':'_address','type':'address'},{'internalType':'bytes32','name':'_messageHash','type':'bytes32'},{'internalType':'uint8','name':'_v','type':'uint8'},{'internalType':'bytes32','name':'_r','type':'bytes32'},{'internalType':'bytes32','name':'_s','type':'bytes32'}],'name':'isSigned','outputs':[{'internalType':'bool','name':'','type':'bool'}],'stateMutability':'pure','type':'function'},{'inputs':[{'internalType':'bytes32','name':'_messageHash','type':'bytes32'},{'internalType':'uint8','name':'_v','type':'uint8'},{'internalType':'bytes32','name':'_r','type':'bytes32'},{'internalType':'bytes32','name':'_s','type':'bytes32'}],'name':'recoverAddress','outputs':[{'internalType':'address','name':'','type':'address'}],'stateMutability':'pure','type':'function'}]
+    this.signatureVerifierContractAddress = '0x4Bf205fAb0BDaD0d2803b5f082E74bE53949FB41'
   }
 
   async sign (data, privateKey) {
@@ -24,6 +26,29 @@ class TwalaSigner {
       s: s
     }
   }
+
+  async isSigned (publicKey, messageHash, v, r, s) {
+    const signatureVerifierContractAbi = this.signatureVerifierContractAbi
+    const signatureVerifierContractAddress = this.signatureVerifierContractAddress
+    const signatureVerifierContract = new this.web3.eth.Contract(signatureVerifierContractAbi, signatureVerifierContractAddress)
+    let isSigned = null
+    try {
+      isSigned = await signatureVerifierContract.methods.isSigned(publicKey, messageHash, v, r, s).call()
+    } catch (error) {
+      isSigned = false
+    }
+
+    return isSigned
+  }
+
+  async recoverAddress (messageHash, v, r, s) {
+    const signatureVerifierContractAbi = this.signatureVerifierContractAbi
+    const signatureVerifierContractAddress = this.signatureVerifierContractAddress
+    const signatureVerifierContract = new this.web3.eth.Contract(signatureVerifierContractAbi, signatureVerifierContractAddress)
+    const address = await signatureVerifierContract.methods.recoverAddress(messageHash, v, r, s).call()
+
+    return address
+  }
 }
 
-module.exports = TwalaSigner
+module.exports = TwalaUtil
